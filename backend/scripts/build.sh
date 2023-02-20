9674fa9dcc9c165de6d491fb35404d0f8c1833a5
okabe <923048992@qq.com>
2023-02-20 14:45:01 +0800

    fix dockerfile

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -1,16 +1,10 @@
-go env -w GOOS=linux
-go build -o ../build/app/app ../main.go
-
-script_dir="$(pwd)"
+#script_dir="$(pwd)"
 
 rm -rf ../data/*
 
-cd ../build/app || exit
-docker build -t app_img .
+cd .. && docker build -t app_img .
 
-cd "$script_dir" || exit
-
-cd ../build/mysql || exit
+cd ./build/mysql || exit
 docker build -t mysql_img .
 
 
